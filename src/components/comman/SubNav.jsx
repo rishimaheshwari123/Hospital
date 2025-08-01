d6a384653565fc08f0e730a71761421fd7cab4c4
Rishi <rishimaheshwari040@gmail.com>
2025-08-01 18:08:33 +0530

    add fax number in  subbar

--- src/components/comman/SubNav.jsx
+++ src/components/comman/SubNav.jsx
@@ -1,7 +1,9 @@
+import { FaFax, FaPhoneAlt } from "react-icons/fa";
+
 export default function SubNavbar() {
   return (
     <div className="fixed bg-white/90 backdrop-blur-sm top-0 left-0 right-0 z-50">
-      <div className="max-w-[98%] mx-auto flex flex-wrap justify-between items-center px-4 py-1">
+      <div className="max-w-[99%] mx-auto flex flex-wrap justify-between items-center px-4 py-1">
         {/* Addresses */}
         <div className="flex flex-col md:flex-row gap-2 md:gap-3">
           <a
@@ -32,13 +34,20 @@ export default function SubNavbar() {
         </div>
 
         {/* Contact & Buttons */}
-        <div className="mt-2 md:mt-0 flex items-center gap-4">
+        <div className="mt-2 md:mt-0 flex items-center gap-2">
           <a
             href="tel:4077678554"
-            className="bg-white text-black px-4 py-1 rounded-lg shadow font-semibold"
+            className="flex items-center space-x-2 bg-white text-black px-4 py-1 rounded-lg shadow font-semibold"
           >
-            (407) 767-8554
+            <FaPhoneAlt className="text-[#00BFB3]" />
+            <span>(407) 767-8554</span>
           </a>
+
+          {/* Fax */}
+          <div className="flex items-center space-x-2 bg-white text-black px-4 py-1 rounded-lg shadow font-semibold">
+            <FaFax className="text-[#00BFB3]" />
+            <span>(407)-767-9121</span>
+          </div>
           <a
             href="https://mycw240.ecwcloud.com/portal28068/jsp/100mp/login_otp.jsp"
             target="_blank"
